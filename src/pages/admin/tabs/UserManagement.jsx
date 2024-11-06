5f3a3b5b45c237ac394da3739df43cc03043c9b7
Fahil M. Namia <150934164+Mimozark-blip@users.noreply.github.com>
2024-11-07 06:53:57 +0800

    New Design Update

--- src/pages/admin/tabs/UserManagement.jsx
+++ src/pages/admin/tabs/UserManagement.jsx
@@ -25,7 +25,7 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 // Sample User Data
 const users = [
@@ -87,12 +87,10 @@ const UserManagement = () => {
   return (
     <div className="p-6 bg-white rounded-xl h-auto w-auto">
       {/* Title */}
-      <div className="text-primary text-[32px] font-bold py-2">
-        User Management
-      </div>
-      <div className="gap-x-2 lg:gap-x-4 flex py-2">
+      <div className="text-primary text-[32px] font-bold">User Management</div>
+      <div className="flex my-4 mb-6 gap-x-[10px]">
         {/* Search Bar */}
-        <div className="relative flex-1 gap-8">
+        <div className="relative w-full">
           <Input
             type="text"
             placeholder="Search..."
@@ -101,89 +99,81 @@ const UserManagement = () => {
           <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-primary scale-75" />
         </div>
         {/* Create Button */}
-        <div className="flex-none">
-          <Button className="ml-4 px-4 md:px-4 lg:px-12 text-secondary rounded-xl">
-            <Plus color="#ffffff" />
-            Create New User
-          </Button>
-        </div>
+        <Button className="text-[16px] font-normal text-secondary rounded-xl">
+          <Plus color="#ffffff" />
+          Create New User
+        </Button>
       </div>
 
       {/* Table */}
-      <div className="py-4">
-        <ScrollArea className=" whitespace-nowrap rounded-md border">
-          <Table>
-            <TableHeader className="bg-primary">
-              <TableRow>
-                <TableHead className="text-secondary sm:px-4 md:px-4 lg:pl-12">
-                  Role
-                </TableHead>
-                <TableHead className="text-secondary">Name</TableHead>
-                <TableHead className="text-secondary">Email</TableHead>
-                <TableHead className="text-secondary sm:px-4 md:px-4 lg:pl-12"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {users.map((user, index) => (
-                <TableRow
-                  key={user.email}
-                  className={`${
-                    index % 2 === 0 ? "bg-gray-50" : "bg-secondary"
-                  } pr-10`}
-                >
-                  <TableCell className="font-medium  sm:px-4 md:px-4 lg:pl-12">
-                    {user.role}
-                  </TableCell>
-                  <TableCell className="font-medium  px-4">
-                    {user.name}
-                  </TableCell>
-                  <TableCell className="font-medium  px-4">
-                    {user.email}
-                  </TableCell>
+      <ScrollArea className=" whitespace-nowrap ">
+        <Table>
+          <TableHeader className="bg-primary">
+            <TableRow>
+              <TableHead className="text-secondary text-[16px] font-normal">
+                Role
+              </TableHead>
+              <TableHead className="text-secondary text-[16px] font-normal">
+                Name
+              </TableHead>
+              <TableHead className="text-secondary text-[16px] font-normal">
+                Email
+              </TableHead>
+              <TableHead className="text-secondary text-[16px] font-normal"></TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {users.map((user, index) => (
+              <TableRow
+                key={user.email}
+                className={`${
+                  index % 2 === 0 ? "bg-[#FAFCFF]" : "bg-secondary"
+                } pr-10`}
+              >
+                <TableCell className="text-[16px] py-1">{user.role}</TableCell>
+                <TableCell className="text-[16px] py-1">{user.name}</TableCell>
+                <TableCell className="text-[16px] py-1">{user.email}</TableCell>
 
-                  <TableCell className="font-medium text-primary sm:w-16 sm:p-2 md:w-16 md:p-2 lg:w-20 lg:pr-8">
-                    <div className="w-full h-auto flex justify-center items-center">
-                      {/* Pop Up Button */}
-                      <Popover>
-                        <PopoverTrigger asChild>
+                <TableCell className="font-medium text-primary sm:w-16 sm:p-2 md:w-16 md:p-2 lg:w-20 lg:pr-8">
+                  <div className="w-full h-auto flex justify-center items-center">
+                    {/* Pop Up Button */}
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button variant="ghost" className="rounded-full p-3">
+                          <EllipsisVertical className="text-primary scale-125" />
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent
+                        side="left"
+                        align="start"
+                        className="w-40 p-0 mt-2 rounded-xl border border-primary"
+                      >
+                        <div className="grid">
                           <Button
-                            variant="ghost"
-                            className="rounded-full p-3 hover:bg-[#edede9]"
+                            variant="secondary"
+                            className="bg-white text-primary rounded-none rounded-t-xl text-[12px] border border-b-primary flex justify-start"
                           >
-                            <EllipsisVertical />
+                            <Pencil className="scale-80" />
+                            Edit
                           </Button>
-                        </PopoverTrigger>
-                        <PopoverContent
-                          side="left"
-                          align="start"
-                          className="w-40 p-0 rounded-xl border border-primary"
-                        >
-                          <div className="grid">
-                            <Button
-                              variant="secondary"
-                              className="bg-white text-primary rounded-none rounded-t-xl text-[12px] border border-b-primary flex justify-start"
-                            >
-                              <Pencil className="scale-80" />
-                              Edit
-                            </Button>
-                            <Button
-                              variant="secondary"
-                              className="bg-white text-primary rounded-none rounded-b-xl text-[12px] flex justify-start"
-                            >
-                              <Trash className="scale-80" />
-                              Delete
-                            </Button>
-                          </div>
-                        </PopoverContent>
-                      </Popover>
-                    </div>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </ScrollArea>
-      </div>
+                          <Button
+                            variant="secondary"
+                            className="bg-white text-primary rounded-none rounded-b-xl text-[12px] flex justify-start"
+                          >
+                            <Trash className="scale-80" />
+                            Delete
+                          </Button>
+                        </div>
+                      </PopoverContent>
+                    </Popover>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
     </div>
   );
 };
