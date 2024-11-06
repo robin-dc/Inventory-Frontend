69e05a2ad796274a9c3b21f8e1c97f2655210b05
Jam <jam.dev@profitlift.co>
2024-11-07 00:27:36 +0800

    fix bug

--- src/pages/admin/components/DeleteConfirmationModal.jsx
+++ src/pages/admin/components/DeleteConfirmationModal.jsx
@@ -8,19 +8,15 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-<<<<<<<<< Temporary merge branch 1
-const DeleteConfirmationModal = () => {
-=========
 
 const DeleteConfirmationModal = ({ confirmDelete, text = "Are you sure you want to delete this item?" }) => {
->>>>>>>>> Temporary merge branch 2
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="outline">Delete</Button>
       </DialogTrigger>
-      <DialogContent className="w-[350px] h-[400px] flex flex-col items-center justify-center"> 
-        
+      <DialogContent className="w-[350px] h-[400px] flex flex-col items-center justify-center">
+
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="120"
@@ -31,54 +27,36 @@ const DeleteConfirmationModal = ({ confirmDelete, text = "Are you sure you want 
           strokeWidth=".50"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className="mr-4 my-2.5"  
+          className="mr-4 my-2.5"
         >
           <circle cx="12" cy="12" r="10" />
           <path d="m15 9-6 6" />
           <path d="m9 9 6 6" />
         </svg>
-       
+
         <DialogTitle className="text-center">Are you sure?</DialogTitle>
         <DialogDescription className="text-center">
-<<<<<<<<< Temporary merge branch 1
-          Are you sure you want to delete this item? <br /> This action cannot
-          be undone.
-        </DialogDescription>
-        <DialogFooter className="flex justify-between">
-          <Button
-            variant="outline"
-            className="border-2 border-gray-500 mx-3 hover:bg-gray-400"
-          >
-            Cancel
-          </Button>
-
-          <Button variant="destructive" className="bg-red-500 text-white mx-3">
-            Delete
-          </Button>
-        </DialogFooter>
-=========
           { text } <br />  This action cannot be undone.
         </DialogDescription>
         <DialogFooter className="flex justify-between">
-       
+
         <Button
           variant="outline"
-          className="border-2 border-gray-500 mx-3 hover:bg-gray-400" 
+          className="border-2 border-gray-500 mx-3 hover:bg-gray-400"
         >
           Cancel
         </Button>
-        
+
         <Button
         onClick={confirmDelete}
           variant="destructive"
-          className="bg-red-500 text-white mx-3"  
+          className="bg-red-500 text-white mx-3"
         >
           Delete
         </Button>
       </DialogFooter>
->>>>>>>>> Temporary merge branch 2
       </DialogContent>
-      
+
     </Dialog>
   );
 };
